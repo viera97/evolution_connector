bd593e00efeef13a6089ac8001ebcc1e504bd835
Viera97 <d.viera1997@gmail.com>
2025-09-20 12:20:10 -0400

    Functional bot
    Added a dictionary of bots for pre initialize instances, in that way the
    systme dosen't have to wait for the initializations. There are 3 idle bots.

--- chat_bot.py
+++ chat_bot.py
@@ -1,15 +1,18 @@
 from fastchat import Fastchat
 import asyncio
 
-async def chating(query):
-    chat: Fastchat = Fastchat()
-    await chat.initialize()
-    
+async def initialize() -> Fastchat:
+    bot: Fastchat = Fastchat()
+    await bot.initialize()
+    print(bot)
+    return bot
+
+async def chating(bot:Fastchat, query: str) -> str :
     response = ""
-    async for step in chat(query):
+    async for step in bot(query):
         if step.type == "response" and step.response is not None:
             response += step.response
     return response
 
 if __name__ == "__main__":
-    asyncio.run(chating("Quiero una cita"))
+    asyncio.run(chating(asyncio.run(initialize()) ,"Quiero una cita"))
--- handle_messages.py
+++ handle_messages.py
@@ -1,9 +1,10 @@
 import uuid
 import supabase_connector
 import chat_bot
+from fastchat import Fastchat
 import asyncio
 
-def format_message(data):
+def format_message(data:dict) -> dict:
     # Formato requerido para el campo message (jsonb)
     return {
         #TODO Ver cuando el bot envíe algo cómo identificarlo
@@ -12,12 +13,12 @@ def format_message(data):
         "additional_kwargs": {},
         "response_metadata": {}
     }
-    
-def save_message(data):
+
+def save_message(data:dict):
     data = format_message(data)
     conversation_id = str(uuid.uuid4())
     supabase_connector.add_conversation_history(conversation_id, data)
 
-def get_chatbot_response(data):
+def get_chatbot_response(bot:Fastchat, data:dict):
     query = data.get("message", {}).get("conversation", "")
-    return asyncio.run(chat_bot.chating(query))
+    return asyncio.run(chat_bot.chating(bot, query))
